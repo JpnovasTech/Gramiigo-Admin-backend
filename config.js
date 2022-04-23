9c7566bf26c23be4b70da5117464b9275715151b
sandesh <sandeshvanwadi@gmail.com>
2022-04-24 03:22:40 +0530

    Admin initial setup

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'secretKey' : '1234-5678',
+    'mongoUrl' : 'mongodb+srv://sandeshvanwadi:Sandy123@cluster0.75a4u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    'PORT' :4000
+}
